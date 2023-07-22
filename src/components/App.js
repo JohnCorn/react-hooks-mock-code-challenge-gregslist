65ef42f962cddc93c44e541d90809f54edfdc807
John Corn <jcornacchioli@gmail.com>
2023-07-22 12:23:57 -0400

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(response => response.json())
+    .then(data => {
+      console.log("data", data);
+      setListings(data);
+    })
+  }, []);
+
+
+  function onRemoveListing(itemID)
+  {
+    console.log("App onRemoveListing")
+
+    fetch(`http://localhost:6001/listings/${itemID}`, 
+    { 
+      method: 'DELETE' 
+    })
+    .then(() =>console.log('Delete successful'));
+  }
+
+  const displayedListings = listings.filter((listing) =>
+    listing.description.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header setSearch={setSearch}/>
+      <ListingsContainer 
+      listings={displayedListings}
+      onRemoveListing={onRemoveListing}
+      />
     </div>
   );
 }
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ListingCard() {
+function ListingCard({ listing,  onRemoveListing}) {
+  const [like, setLike] = useState(false);
+
+  function HandleRemove()
+  {
+    onRemoveListing(listing.id);
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img 
+        src={listing.image} 
+        alt={listing.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {like ? (
+          <button 
+          onClick={() => setLike(false)}
+          className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button 
+          onClick={() => setLike(true)}
+          className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{listing.description}</strong>
+        <span> · {listing.location}</span>
+
+        <button 
+          onClick={HandleRemove}
+          className="emoji-button delete">🗑️
+        </button>
       </div>
     </li>
   );
